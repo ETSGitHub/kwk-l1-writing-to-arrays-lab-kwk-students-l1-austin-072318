f389d3153750acd87529bfe59a748dbfcdc8cec3
ETSGitHub <etschulte@gmail.com>
2018-07-25 13:56:31 +0000

    automatically backed up by learn

--- editing_arrays.rb
+++ editing_arrays.rb
@@ -5,9 +5,16 @@ rainbow_colors = [
 ]
 
 def change_rainbow_colors
-  # Write a solution that corrects the three colors in rainbow_colors, then returns the corrected array
+  rainbow_colors[0] = "red"
+  puts change_rainbow_colors[0]
+  rainbow_colors[1] = "light_red"
+  puts change_rainbow_colors[1]
+  rainbow_colors[2] = "light_yellow"
+  puts change_rainbow_colors[2]
 end
 
 def add_colors
-  # Write a solution that adds "green", then "blue" to the rainbow_colors array, then returns the array
+ rainbow_colors.push("green","blue")
 end
+change_rainbow_colors
+add_colors
