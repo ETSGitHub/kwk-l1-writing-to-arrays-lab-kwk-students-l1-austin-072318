58d9cd3ad7e44d4f108f3ca37de5a8619eda0078
ETSGitHub <etschulte@gmail.com>
2018-07-25 14:11:22 +0000

    automatically backed up by learn

--- editing_arrays.rb
+++ editing_arrays.rb
@@ -4,7 +4,14 @@ rainbow_colors = [
   "light_cyan"
 ]
 
-def change_rainbow_colors(rainbow_colors)
+def change_rainbow_colors
+  
+  rainbow_colors = [
+  "yellow",
+  "default",
+  "light_cyan"
+]
+  
   rainbow_colors[0] = "red"
   puts change_rainbow_colors[0]
   rainbow_colors[1] = "light_red"
